953e9318a1a05c7d08fa2b9ac8971e71bd0894a6
phaneendra <phani.manthena27@gmail.com>
2023-05-12 15:50:36 +0530

    Project-files added

---
+++ execute.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Save the current working directory
+current_dir=$(pwd)
+
+# Define the list of directories to process
+directories=(
+    /root/RoboShop-Micro-Services-Project-/cart
+    /root/RoboShop-Micro-Services-Project-/catalogue
+    /root/RoboShop-Micro-Services-Project-/mongodb
+    /root/RoboShop-Micro-Services-Project-/mysql
+    /root/RoboShop-Micro-Services-Project-/payment
+    /root/RoboShop-Micro-Services-Project-/rabbitmq
+    /root/RoboShop-Micro-Services-Project-/redis
+    /root/RoboShop-Micro-Services-Project-/shipping
+    /root/RoboShop-Micro-Services-Project-/user
+    /RoboShop-Micro-Services-Project-/web
+    
+
+# Loop through each directory and execute the kubectl apply commands
+for dir in "${directories[@]}"
+do
+  echo "Processing directory: $dir"
+  cd "$dir"
+  kubectl apply -f manifest.yml
+done
+
+# Return to the original directory
+cd "$current_dir"
